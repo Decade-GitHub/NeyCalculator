cf7fbfea44aebd03a6e2dafe9546cb90c0edb705
Matheus Moreira <jasperwantstoplay@gmail.com>
2023-11-21 17:19:48 -0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NeyCalcGUI
+{
+    public partial class MainWindow : Window
+    {
+        
+        const string ErrorMessage = "Tu é burro, irmão?";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int UserUnit = InputDropBox.SelectedIndex;
+            if (!ValidateUserValueInput())
+            {
+                ResultBox.Text = ErrorMessage;
+                return;
+            }
+            try
+            {
+                float UserValue = TransformUserValue();
+                float Result = Master.Calculate(UserValue, UserUnit);
+                ResultBox.Text = Result.ToString();
+
+                string ResultType = ReturnTypeExpression();
+                ResultTypeBox.Text = ResultType;
+            }
+            catch (Exception ex)
+            {
+                ResultBox.Text = ex.Message;
+            }
+        }
+        private bool ValidateUserValueInput()
+        {
+            if (string.IsNullOrEmpty(InputTextBox.Text))
+            {
+                return false;
+            }
+            else if (!float.TryParse(InputTextBox.Text, out _))
+            {
+                return false;
+            }
+            return true;
+        }
+        private float TransformUserValue()
+        {
+            _ = float.TryParse(InputTextBox.Text, out float UserValue);
+            return UserValue;
+        }
+        private string ReturnTypeExpression()
+        {
+            int UserUnit = InputDropBox.SelectedIndex;
+
+            return UserUnit switch
+            {
+                0 => "Neymeters",
+                1 => "Neykilos",
+                2 => "Neyliters",
+                3 => "Neykilobytes",
+                4 => "Neykilometers per hour",
+                _ => throw new ArgumentOutOfRangeException("What the fuck")
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Windows.Controls;
+using System.Xml.Serialization;
+
+namespace NeyCalcGUI
+{
+    class Master
+    {
+        private string? UserValue;
+        private int UserUnit;
+
+        const int AlturaNeymar = 175;
+        const int MassaNeymar = 63;
+        const int CapacidadeNeymar = 5;
+        const int KilobytesNeymar = 206;
+        const int VelocidadeNeymar = 30;
+        public static float Calculate(float UV, int UU)
+        {
+            return UU switch
+            {
+                0 => UV / AlturaNeymar,
+                1 => UV / MassaNeymar,
+                2 => UV / CapacidadeNeymar,
+                3 => UV / KilobytesNeymar,
+                4 => UV / VelocidadeNeymar,
+                _ => throw new InvalidDataException("Tu é burro, irmão?")
+            };
+        }
+    }
+}
