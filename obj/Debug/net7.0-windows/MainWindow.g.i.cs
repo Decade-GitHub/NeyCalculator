8de2f988b0585ed5ac040505b19e3d861a4b41d2
Matheus Moreira <jasperwantstoplay@gmail.com>
2023-11-21 17:24:54 -0300

    Initial commit

--- obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace NeyCalcGUI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/NeyCalcGUI;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NeyCalcGUI;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
